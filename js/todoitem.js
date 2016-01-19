3dca1070fc870de920526e185c26c7d051d8ee80
rulotr <raul20xx@hotmail.com>
2016-01-19 17:40:02 -0600

    Create todoitem.js

---
+++ js/todoitem.js
@@ -0,0 +1,17 @@
+(function () {
+	'use strict';
+	TodoMVC.Controls.TodoItem = React.createClass({
+			render: function(){
+				return(
+								<li>
+									<div className="view">
+										<input	className="toggle" type="checkbox" />
+										<label>Tarea 1</label>
+										<button className="destroy"  />
+									</div>
+									<input 	className="edit" />
+								</li>
+					);
+			}
+	});	
+})();
