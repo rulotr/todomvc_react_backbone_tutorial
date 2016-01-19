6ebc77f062000f4333f8853b99f089dec01cfa50
rulotr <raul20xx@hotmail.com>
2016-01-19 17:39:37 -0600

    Create footes.js

---
+++ js/footes.js
@@ -0,0 +1,22 @@
+(function(){
+	'use strict'
+	TodoMVC.Controls.TodoFooter = React.createClass({
+		render: function(){
+			return(
+					<footer id="footer">
+						<span id="todo-count">
+							<strong>1 </strong> item left
+						</span>
+						<ul id="filters">
+							<li><a	href="#/">All</a></li>
+							<span> </span>
+							<li><a	href="#/active">Active</a></li>
+							<span> </span>
+							<li><a	href="#/completed">	Completed</a></li>
+						</ul>
+	                    <button	id="clear-completed"> Clear completed </button>		
+				   	</footer>
+				);
+		}
+	});
+})();
